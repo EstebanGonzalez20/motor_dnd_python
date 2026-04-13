89288f047b596566943ef718c7bb2e886f6a7027
EstebanGonzalez20 <gonzavenom@gmail.com>
2026-04-13 15:58:01 -0300

    Crea el archivo position.py

---
+++ engine/components/position.py
@@ -0,0 +1,27 @@
+from engine.component import Component
+from dataclasses import dataclass
+
+@dataclass
+class Position(Component):
+    """
+    Esta clase permite le da una ubicación a la entidad
+    dentro de un mapa.
+    """
+    _x: int
+    _y: int
+
+    @property
+    def x(self) -> int:
+        return self._x
+    
+    @x.setter
+    def x(self, value: int):
+        self._x = value
+
+    @property
+    def y(self) -> int:
+        return self._y
+    
+    @y.setter
+    def y(self, value: int):
+        self._y = value
