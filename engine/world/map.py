fc53b0435823fcb7fa910f6d1969e75a061268dc
EstebanGonzalez20 <gonzavenom@gmail.com>
2026-04-13 17:22:13 -0300

    Crea el archivo map.py y su lógica

---
+++ engine/world/map.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from components.position import Position
+from engine.entity import Entity
+
+@dataclass
+class Map:
+    """
+    Esta clase representa a un tablero
+    """
+    width: int
+    height: int
+    entities: dict[Position, Entity]
+
+    def distance(self, pos1: Position, pos2: Position) -> int:
+        """
+        Calcula la distancia entre dos posiciones del mapa
+        """
+        return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
+    
+    def is_occupied(self, position: Position) -> bool:
+        """
+        Consulta si una posición del mapa esta ocupada
+        """
+        return position in self.entities
