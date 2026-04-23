84cd168257ebdc019b1e53dd0255912ad4d7fa1f
EstebanGonzalez20 <gonzavenom@gmail.com>
2026-04-23 18:11:35 -0300

    Agrega métodos para manejar entidades en el mapa

--- engine/world/map.py
+++ engine/world/map.py
@@ -22,3 +22,24 @@ class Map:
         Consulta si una posición del mapa esta ocupada
         """
         return position in self.entities
+    
+    @property
+    def entities(self):
+        return self.entities
+
+    def add_entity(self, position: tuple[int, int], entity: Entity) -> None:
+        """
+        Agrega una entidad al mapa en un espacio vacío
+        """
+        if position not in self.entities.keys():
+            self.entities[position] = entity
+
+        else:
+            raise ValueError("The entity must be placed in an empty space")
+        
+    def remove_entity(self, position: tuple[int, int]) -> None:
+        if position in self.entities.keys():
+            self.entities.pop(position)
+
+        else:
+            raise ValueError("The position specified does'nt contains an entity")
