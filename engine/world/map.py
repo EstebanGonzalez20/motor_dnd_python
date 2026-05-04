3e49a7b85a47d0b7345b4e0739894696d9b242cf
EstebanGonzalez <gonzavenom@gmail.com>
2026-05-04 16:48:19 -0300

    Arregla tipo de datos de algunas funciones

--- engine/world/map.py
+++ engine/world/map.py
@@ -9,7 +9,7 @@ class Map:
     """
     width: int
     height: int
-    entities: dict[tuple[Position], Entity]
+    _entities: dict[tuple[int, int], Entity]
 
     def distance(self, pos1: Position, pos2: Position) -> int:
         """
@@ -17,7 +17,7 @@ class Map:
         """
         return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
     
-    def is_occupied(self, position: tuple) -> bool:
+    def is_occupied(self, position: tuple[int, int]) -> bool:
         """
         Consulta si una posición del mapa esta ocupada
         """
@@ -25,13 +25,13 @@ class Map:
     
     @property
     def entities(self):
-        return self.entities
+        return self._entities
 
     def add_entity(self, position: Position, entity: Entity) -> None:
         """
         Agrega una entidad al mapa en un espacio vacío
         """
-        position_key = tuple(position)
+        position_key = position.tuple_position
         if position_key not in self.entities.keys():
             self.entities[position_key] = entity
 
@@ -39,7 +39,7 @@ class Map:
             raise ValueError("The entity must be placed in an empty space")
         
     def remove_entity(self, position: Position) -> None:
-        position_key = tuple(position)
+        position_key = position.tuple_position
         if position_key in self.entities.keys():
             self.entities.pop(position_key)
 
