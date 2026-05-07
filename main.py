93e6d8c18a4628f42dfcff480ab8e94d26d04561
EstebanGonzalez <gonzavenom@gmail.com>
2026-05-07 18:06:20 -0300

    refacture/Modifica main para usar CharaterFactory

--- main.py
+++ main.py
@@ -2,21 +2,13 @@ from engine.world.map import Map
 from game.game_state import GameState
 from game.game_controller import GameController
 from ui.console.console_ui import ConsoleUI
-from engine.entity import Entity
 from engine.components.position import Position
-from engine.components.speed import Movement
+from game.factories.character_factory import CharacterFactory
 import os
 
-def crear_personaje() -> Entity:
-    os.system("cls")
-    name = input("Ingrese el nombre de jugador: ")
-    player = Entity(1, name)
-    player.add(Movement(6))
-    player.add(Position(5, 5))
-    return player
-
 def main():
-    player = crear_personaje()
+    os.system("cls")
+    player = CharacterFactory.create_player(1)
     game_map = Map(9, 9,{})
     game_map.add_entity(player.get(Position), player)
     state = GameState(game_map)
